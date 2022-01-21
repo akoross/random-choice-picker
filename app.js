d1d197b933f6cf87528ddc0e24ac119d31bf7555
Sergii <74363342+akoross@users.noreply.github.com>
2022-01-21 21:10:34 +0200

    initial

---
+++ app.js
@@ -0,0 +1,70 @@
+const tagsEl = document.getElementById('tags');
+const textarea = document.getElementById('textarea');
+
+textarea.focus();
+
+const createTags = (input) => {
+  const tags = input
+    .split(',')
+    .filter((tag) => tag.trim() !== '')
+    .map((tag) => tag.trim());
+
+  tagsEl.innerHTML = '';
+
+  tags.forEach((tag) => {
+    const tagEl = document.createElement('span');
+    tagEl.classList.add('tag');
+    tagEl.innerText = tag;
+    tagsEl.appendChild(tagEl);
+  });
+};
+
+const randomSelect = () => {
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    highlightTag(randomTag);
+
+    setTimeout(() => {
+      unHighlightTag(randomTag);
+    }, 100);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    if (pickRandomTag()) {
+      setTimeout(() => {
+        const randomTag = pickRandomTag();
+
+        highlightTag(randomTag);
+      }, 100);
+    }
+  }, times * 100);
+};
+
+const pickRandomTag = () => {
+  const tags = document.querySelectorAll('.tag');
+
+  return tags[Math.floor(Math.random() * tags.length)];
+};
+
+const highlightTag = (tag) => {
+  tag.classList.add('highlight');
+};
+
+const unHighlightTag = (tag) => {
+  tag.classList.remove('highlight');
+};
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value);
+
+  if (e.key === 'Enter') {
+    if (document.querySelectorAll('.tag').length) randomSelect();
+    setTimeout(() => {
+      e.target.value = '';
+    }, 10);
+  }
+});
